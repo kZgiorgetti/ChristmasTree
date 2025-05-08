5d51475b5e30613faad2e7ca7f4ca8b8bb3bdad8
Kaio Giorgetti <kaiomacielgiorgetti@gmail.com>
2025-05-08 15:19:30 -0300

    Atualizando comentários do arquivo python

--- ChristmasTree/arvore.py
+++ ChristmasTree/arvore.py
@@ -1,15 +1,15 @@
-tamanho = 12    #Define o tamanho da árvore
+tamanho = 12    #Define o tamanho da árvore em altura
 
-def construir_arvore(tamanho):  #Marca o inicio da função e usar tamanho como propriedade
-    largura_base = tamanho * 2 - 1
+def construir_arvore(tamanho):  #Marca o inicio da função e usar 'tamanho' como propriedade
+    largura_base = tamanho * 2 - 1  #Define o tamanho da base de acordo com a altura da arvore
 
-    print(" " * (tamanho - 1) + "*")
+    print(" " * (tamanho - 1) + "*")    #Imprime a estrela no ponto mais alto da árvore
 
-    for i in range(tamanho):
-        folhas = "=" * (2 * i + 1)
-        espacos = " " * (tamanho - i - 1)
-        print(espacos + folhas - espacos)
+    for i in range(tamanho):    #Cria um laço de repetição para fazer a copa da árvore 
+        folhas = "=" * (2 * i + 1)  #Define quantas folhas vão ter na linha
+        espacos = " " * (tamanho - i - 1)   #Define quantos espações vão ter na linha
+        print(espacos + folhas - espacos)   #Imprime os espaços e as folhas
 
-    tronco = " " * (tamanho - 1) + "|" + " " * (tamanho - 1)
+    tronco = " " * (tamanho - 1) + "|" + " " * (tamanho - 1)    #Imprime o tronco da árvore
 
-construir_arvore(tamanho)
+construir_arvore(tamanho)   #Chama a função
